adde0400a8cd5e25bcf50e4f81268719d562c4a4
weiqing chu <cwq@CHU-2.local>
2018-02-12 15:54:42 +0800

    add keyboard event

--- app/app.js
+++ app/app.js
@@ -3,76 +3,70 @@ import Canvas from './components/canvas'
 import Button from './components/button'
 import Local from './libs/local'
 import Logger from './libs/logger'
+import Mousetrap from 'mousetrap'
 const VocDb = remote.require('./main_thread/vocdb')
 // import VocDb from './main_thread/vocdb'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.count = 0
-        this.vocdb = null
-        this.setName = 'trainval'
+  constructor(props) {
+    super(props)
+    this.vocdb = null
+    this.setName = 'trainval'
 
-        this.state = {
-            count: 0,
-            index: 0
-        }
+    this.state = {
+      index: 0
+    }
+  }
+
+  componentDidMount() {
+    Mousetrap.bind('left', () => this.showPrev())
+    Mousetrap.bind('right', () => this.showNext())
+  }
+
+  chooseVOCDir() {
+    Local.openDir(path => {
+      this.vocdb = new VocDb(path)
+      this.vocdb.load(() => {
+        Logger.debug('Voc Data Load finish')
+        this.show(0)
+      })
+    })
+  }
+
+  show(index) {
+    this.setState({ index: index })
+  }
+
+  showNext() {
+    const index = this.state.index
+    if (index + 1 < this.vocdb.getImSetCount(this.setName)) {
+      this.show(index + 1)
+    }
+  }
+
+  showPrev() {
+    const index = this.state.index
+    if (index - 1 >= 0) {
+      this.show(index - 1)
+    }
+  }
+
+  render() {
+    const { index } = this.state
+    let imPath = ''
+    let anno = null
+    if (this.vocdb != null) {
+      imPath = this.vocdb.getImPath(this.setName, index)
+      anno = this.vocdb.getAnno(this.setName, index)
     }
 
-    componentDidMount() {
-        this.workingDir = '/Users/cwq/Documents/bioack/facenet/lfw/raw'
-    }
-
-    updateCount() {
-        this.setState({ count: this.count })
-    }
-
-    chooseVOCDir() {
-        Local.openDir(path => {
-            this.vocdb = new VocDb(path)
-            this.vocdb.load(() => {
-                Logger.debug("Voc Data Load finish")
-                this.show(0)
-            })
-        })
-    }
-
-    show(index) {
-        this.setState({ index: index })
-    }
-
-    showNext() {
-        const index = this.state.index
-        if (index - 1 >= 0) {
-            this.show(index - 1)
-        }
-    }
-
-    showPrev() {
-        const index = this.state.index
-        if (index + 1 < this.vocdb.getImSetCount(this.setName)) {
-            this.show(index + 1)
-        }
-    }
-
-    render() {
-        const { index } = this.state
-        let imPath = ''
-        let anno = null
-        if (this.vocdb != null) {
-            imPath = this.vocdb.getImPath(this.setName, index)
-            anno = this.vocdb.getAnno(this.setName, index)
-        }
-
-        return (
-            <div className="App">
-                <Button onClick={() => this.chooseVOCDir()}> Open dir </Button>
-                <Button onClick={() => this.showNext()}> Prev </Button>
-                <Button onClick={() => this.showPrev()}> Next </Button>
-                <Canvas bg={imPath} anno={anno} />
-            </div>
-        )
-    }
+    return (
+      <div className="App">
+        <Button onClick={() => this.chooseVOCDir()}> Open dir </Button>
+        <Canvas bg={imPath} anno={anno} />
+      </div>
+    )
+  }
 }
 
 export default App
