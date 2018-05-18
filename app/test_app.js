e0c6a88a712de7d4f36719cfc3460cadb5216c57
sanster <cwq1913@gmail.com>
2018-05-18 18:01:57 +0800

    WIP

---
+++ app/test_app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+
+class App extends React.Component {
+  render() {
+    return (
+      // <Button variant="raised" color="primary">
+      //   Hello World
+      // </Button>
+      <div>Hello</div>
+    )
+  }
+}
+
+export default App
